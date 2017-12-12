4ad9dae2a93d7641609d8c6c1acb21506f4c0bd8
Ahed <ahedqaood19@gmail.com>
2017-12-13 05:42:15 +0800

    only letters will be accepted

--- StringManipulator.php
+++ StringManipulator.php
@@ -5,8 +5,12 @@ class StringManipulator{
     static private $initials = " ";
     // to store an array of the words that make up the received name
     static private $manipulatedString= " ";
+    // to store the input name with letters only
+    static private $onlyAlpha= " ";
 
     static public function firstLetters($name){
+        // remove everything but letters, a new cleaned string will be returned
+        $name = self::removeNonAlpha($name);
         // split the name into an array of words, all in uppercase
         self::$manipulatedString = explode(" ", ucwords($name));
         self::$initials = " "; // to make sure old data is deleted
@@ -14,5 +18,27 @@ class StringManipulator{
             self::$initials = self::$initials . self::$manipulatedString[$i][0];
         }
         return self::$initials;
+    }
+    static public function removeNonAlpha($input){
+        self::$onlyAlpha = "a"; // to make sure old data is deleted
+        // to follow up with onlyAlpha index
+        $alphaIdx = 0;
+        // to make sure only 1 space is among letters
+        $isSpace = true;
+        for ($i = 0; $i < strlen($input); $i++) {
+            if (ctype_alpha($input[$i]) || ( (ctype_space($input[$i])) && !$isSpace ) ) {
+                self::$onlyAlpha[$alphaIdx] = $input[$i];
+                $alphaIdx++;
+                if(ctype_space($input[$i]))
+                    $isSpace = true;
+                else
+                    $isSpace = false;
+            }
+        }
+        return self::$onlyAlpha;
+    }
+    static public function displayInitials($output){
+        print $output;
+        print "\n";
     }
 }
--- testStringManipulator.php
+++ testStringManipulator.php
@@ -1,9 +1,18 @@
 <?php
 include "StringManipulator.php";
 $manipulator = new StringManipulator;
-$testSentence = "International islamic university Malaysia";
-var_dump($manipulator->firstLetters($testSentence));
+
+$testSentence = "S3leem Murhaf 1haddad";
+$manipulator->displayInitials($manipulator->firstLetters($testSentence));
 
 // once again to make sure there's no old data stored in the variable $initials
+$testSentence = " International islamic university Malaysia";
+$manipulator->displayInitials($manipulator->firstLetters($testSentence));
+
 $testSentence = "Ahid Naif alqaud";
-var_dump($manipulator->firstLetters($testSentence));
+$manipulator->displayInitials($manipulator->firstLetters($testSentence));
+
+$testSentence = "?Hello 1    World";
+$manipulator->displayInitials($manipulator->firstLetters($testSentence));
+
+//print $manipulator->removeNonAlpha(" Hello World Hi me");
